9ce41578091732069a19cb9eb74c48d75f7db27b
Tomoms <fondatommaso2@gmail.com>
2020-04-13 21:24:20 +0200

    Ready for submission

--- src/esercizio-C-2020-04-06.c
+++ src/esercizio-C-2020-04-06.c
@@ -1,8 +1,33 @@
-/*
- * esercizio-C-2020-04-06.c
- *
- *  Created on: Apr 2, 2020
- *      Author: marco
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+void find_brute_force(int *ptr, int size)
+{
+	int x;
+	bool duplicate;
+	bool visited[size];
+	memset(visited, 0, size * sizeof(bool));
+	for (int i = 0; i < size; i++) {
+		x = ptr[i];
+		if (!visited[i]) {
+			duplicate = 0;
+			for (int j = i + 1; j < size; j++) {
+				if (x == ptr[j]) {
+					duplicate = 1;
+					visited[j] = 1;
+				}
+			}
+			if (!duplicate)
+				printf("The element that appears only once is: %d\n", x);
+		}
+	}
+}
 
+int main (int argc, char *argv[])
+{
+	int a[] = {1, 5, 6, 2, 1, 6, 4, 3, 2, 5, 3};
+	find_brute_force(a, 11);
+	return 0;
+}
